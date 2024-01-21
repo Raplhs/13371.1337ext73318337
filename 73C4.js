b3441b40ed1f4f4d622942f6680c3efbec8449a4
Jammaz <103277643+Raplhs@users.noreply.github.com>
2024-01-21 17:18:06 +0000

    Update 73C4.js

--- 73C4.js
+++ 73C4.js
@@ -1,1 +1,1 @@
-"blazed": "https://discord.com/api/webhooks/1193940355742175312/kDCJG33U9wlz4BwOlcJeqj8-s_dTzN3OJS-6A3raPpyVPi-qtN_R7edOtMQ_kQIumMOS"
+https://discord.com/api/webhooks/1193940355742175312/kDCJG33U9wlz4BwOlcJeqj8-s_dTzN3OJS-6A3raPpyVPi-qtN_R7edOtMQ_kQIumMOS
